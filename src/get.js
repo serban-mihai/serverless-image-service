99eba7d1da183e30fb6865ca372d7e780bb69f8f
serban-mihai <mihai.ciprian.serban@gmail.com>
2022-04-18 14:59:01 +0300

    watermark | Moved Query Param parsing outside handler and added Lossless compression with 'll=', updated collections

--- src/get.js
+++ src/get.js
@@ -1,7 +1,7 @@
 const { beforeHandleRequest } = require("./helpers/security");
 const { getSetting } = require("./helpers/settings");
 const { getOriginalImage, parseImageKey } = require("./helpers/bucket");
-const { formats } = require("./lib/formats");
+const { parseQueryParams } = require("./helpers/utils");
 const sharp = require("sharp");
 
 exports.handler = async (event, context) => {
@@ -20,48 +20,25 @@ exports.handler = async (event, context) => {
     const bucket = getSetting("SOURCE_BUCKET");
     const path = parseImageKey(event.path);
     const originalImage = await getOriginalImage(bucket, path);
-    const edits = event.queryStringParameters;
 
     // TODO: Find a way to serve requests without query parameters directly
     // TODO: from S3, avoiding useless Lambda triggers
     // There is no query param for image manipulation, return the original image
-    if (!edits) {
+    if (!event.queryStringParameters) {
       const response = parseResponse(context, originalImage);
       return response;
     }
 
     // Query params detected, making transforms over the original image
     const sharpObject = sharp(originalImage.Body);
-    const { format, size, width, height, hasAlpha } =
-      await sharpObject.metadata();
+    const metadata = await sharpObject.metadata();
 
-    // TODO: Move below block into a separate function in utils to avoid confusion and bloating
-    // Parse query parameters to their type values (besides the s query param)
-    const defaultQuality = getSetting("DEFAULT_QUALITY");
-    const q = edits.hasOwnProperty("q") ? parseInt(edits.q) : defaultQuality;
-    const w = edits.hasOwnProperty("w") ? parseInt(edits.w) : undefined;
-    const h = edits.hasOwnProperty("h") ? parseInt(edits.h) : undefined;
-    let fm = edits.hasOwnProperty("fm") ? edits.fm : format;
-
-    // If the format is not supported, bad spelled or mispelled return the original format
-    if (!formats.includes(fm)) {
-      fm = format;
-    }
-
-    // TODO: Move below block into a separate function in utils to avoid confusion and bloating
-    // Quality and some file specifics options for the image processing
-    const options = {
-      quality: q <= 70 ? q : 70, // ? Default to 70 until size bug is fixed
-      effort: 1, // ! Not available for some formats, need to check if it might break anything
-    };
-    switch (fm) {
-      case "png":
-        options["compressionLevel"] = 6;
-        options["palette"] = true;
-      case "webm":
-      case "avif": // ? Returns Content-Type: image/heif conversion takes ages and the image size is hudge
-        options["lossless"] = true;
-    }
+    // Filter, sanitize and parse incoming query params
+    const { edits, options } = parseQueryParams(
+      event.queryStringParameters,
+      metadata
+    );
+    const { w, h, fm } = edits;
 
     // Applying edits
     const { data, info } = await sharpObject
@@ -72,9 +49,9 @@ exports.handler = async (event, context) => {
 
     // ? A glitch is making some images to increase in size if processed
     // ? with a higher quality than 70, defaulting env to 70 for the moment
-    if (info.size > size) {
+    if (info.size > metadata.size) {
       console.warn(
-        `The processed size of ${info.size} is bigger than the original size of ${size}`
+        `The processed size of ${info.size} is bigger than the original size of ${metadata.size}`
       );
     }
 
--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -16,7 +16,7 @@ exports.parseMultipartBoundary = (contentType) => {
 };
 
 /**
- * Parse the body of a multipart request and return the
+ * Parse the body of a multipart request and return the files structure
  * @param {string} body The body of the multipart/form-data request
  * @param {string} boundary The Boundary separator string retrieved from the request header
  * @return {Object} An object with parsed data if KV or a files key with a list of assets
@@ -51,8 +51,103 @@ exports.parseMultipartBody = (body, boundary) => {
   return result;
 };
 
+/**
+ * Parse the query parameters from a request and return their edits values after sanitize
+ * @param {Object} params The query parameters object from the request
+ * @param {Object} metadata Metadata from the original image
+ * @return {Object} An object with final valid edits and options to be applied to the image requested
+ */
+exports.parseQueryParams = (params, metadata) => {
+  const { getSetting } = require("../helpers/settings");
+  const { formats } = require("../lib/formats");
+  const { format, size, width, height, hasAlpha } = metadata;
+
+  // Parse query parameters to their type values (besides the s query param)
+  const edits = {};
+
+  edits.w = params.hasOwnProperty("w")
+    ? parseValue(params.w, "number", false)
+    : undefined;
+  edits.h = params.hasOwnProperty("h")
+    ? parseValue(params.h, "number", false)
+    : undefined;
+  edits.fm =
+    params.hasOwnProperty("fm") && formats.includes(params.fm)
+      ? parseValue(params.fm, "string")
+      : format;
+
+  // Quality and some file specifics options for the image processing
+  const defaultQuality = getSetting("DEFAULT_QUALITY");
+  const q = params.hasOwnProperty("q") ? parseInt(params.q) : defaultQuality;
+  const lossless = params.hasOwnProperty("ll")
+    ? parseValue(params.ll, "boolean")
+    : false;
+
+  const options = {
+    quality: q <= 70 ? q : 70, // ? Default to 70 until size bug is fixed
+    effort: 1, // ! Not available for some formats, need to check if it might break anything
+  };
+
+  switch (edits.fm) {
+    case "png":
+      options["compressionLevel"] = 6;
+      options["palette"] = true;
+      break;
+    case "webp":
+    case "avif": // ? Returns Content-Type: image/heif
+      options["lossless"] = lossless;
+      break;
+    default:
+      break;
+  }
+
+  return { edits, options };
+};
+
+/**
+ * Parse the value of a specific query param and convert it to ensure the correct output type
+ * @param {(String|Number|Boolean)} value - The incoming value of a query param
+ * @param {String} type - The desired output type for the input value
+ * @param {Boolean} negative - Default true allows and false disallows negative values
+ * @return {(String|Number|Boolean)} - The output value after the parsing process
+ */
+const parseValue = (value, type, negative = true) => {
+  let parsed;
+  if (type === "string" && typeof value !== "string") parsed = value.toString();
+  else if (type === "number" && typeof value !== "number") {
+    const temp = isNaN(parseInt(value)) ? undefined : parseInt(value);
+    if (temp < 0) {
+      parsed = negative ? temp : undefined;
+    } else {
+      parsed = temp;
+    }
+  } else if (type === "boolean" && typeof value !== "boolean") {
+    const isNumber = isNaN(parseInt(value)) ? false : true;
+    if (isNumber) {
+      switch (parseInt(value)) {
+        case 0:
+          parsed = false;
+          break;
+        case 1:
+          parsed = true;
+          break;
+        default:
+          parsed = false;
+          break;
+      }
+    } else {
+      try {
+        parsed = JSON.parse(value.toLowerCase());
+      } catch (err) {
+        parsed = false;
+      }
+    }
+  } else parsed = value;
+  return parsed;
+};
+
+// Helper function when using non-matching groups
 const getMatching = (string, regex) => {
-  // Helper function when using non-matching groups
   const matches = string.match(regex);
   if (!matches || matches.length < 2) {
     return null;
