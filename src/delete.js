4d4aac5568641802a1dbaebe7631620d28f292f5
serban-mihai <mihai.ciprian.serban@gmail.com>
2022-04-20 20:43:23 +0300

    delete-refactor | Added multiple deletes through body param

--- src/delete.js
+++ src/delete.js
@@ -16,14 +16,39 @@ exports.handler = async (event, context) => {
   const bucket = getSetting("SOURCE_BUCKET");
   const key = parseImageKey(event.path);
 
-  // Check if the image already exists under the required path
-  // If exists return a conflict message without attemting to upload
-  const exists = await getMetadata(bucket, key);
-  if (!exists) {
+  const { images } = JSON.parse(event.body);
+
+  // No keys passed to the function's body, return 404
+  if (images.length === 0) {
     const result = {
       status: 404,
       code: "not-found",
-      message: "No image exists under the requested path",
+      message: "No image passed for removal",
+    };
+    console.warn(result);
+    const message = await JSON.stringify(result, 2, null);
+    const response = parseResponse(context, message, result.status);
+    return response;
+  }
+
+  // Check if the image already exists under the required path
+  // If exists return a conflict message without attemting to upload
+  let exists = [];
+  for (const name of images) {
+    const filename = decodeURI(name);
+    const currentKey = `${key}${key === "" ? "" : "/"}${filename}`;
+    const exist = await getMetadata(bucket, currentKey);
+    if (exist) exists.push(filename);
+  }
+
+  if (exists.length !== images.length) {
+    const result = {
+      status: 404,
+      code: "not-found",
+      message: `Images [ ${images
+        .filter((image) => !exists.includes(image))
+        .toString()
+        .replace(/,/g, ", ")} ] don't exist under the requested path /${key}`,
     };
     console.warn(result);
     const message = await JSON.stringify(result, 2, null);
@@ -32,12 +57,16 @@ exports.handler = async (event, context) => {
   }
 
   try {
-    // Detele the image and return a message
-    const removedImage = await removeImage(bucket, key);
+    // Delete imagese and return a message
+    for (const image of images) {
+      const filename = decodeURI(image);
+      const currentKey = `${key}${key === "" ? "" : "/"}${filename}`;
+      await removeImage(bucket, currentKey);
+    }
     const result = {
       status: 200,
       code: "success",
-      message: "Image removed successfully!",
+      message: "Images removed successfully!",
     };
     console.log(result);
     const message = await JSON.stringify(result);
@@ -46,7 +75,7 @@ exports.handler = async (event, context) => {
   } catch (err) {
     console.error(err);
     const parsedError = await JSON.stringify(
-      Object.assign(err, { message: "Image removal failed!" }),
+      Object.assign(err, { message: "Images removal failed!" }),
       2,
       null
     );
